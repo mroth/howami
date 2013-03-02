7b42b971b020c9b4f6d9730451ef783c69a68d54
Matthew Rothenberg <mrothenberg@gmail.com>
2013-03-02 18:19:12 -0500

    storing to file properly

--- lib/howami/configuration.rb
+++ lib/howami/configuration.rb
@@ -4,20 +4,33 @@ module Howami
   class Configuration
     include Methadone::CLILogging
 
+    XDG_CONFIG_HOME = ENV['XDG_CONFIG_HOME'] || File.join(ENV['HOME'], '.config')
+    CREDENTIALS_STORE = File.join( XDG_CONFIG_HOME, "howami", "credentials.yml")
+
     # def initialize
     # end
 
     def self.has_valid_credentials?
+      self.has_stored_credentials?
     end
 
-    def self.store_credentials
+    def self.store_credentials(user_token, user_secret)
+      #create proper XDG directory if doesn't exist
+      FileUtils.mkdir_p( File.dirname(CREDENTIALS_STORE) )
+
+      #store
+      File.open(CREDENTIALS_STORE, 'w') do |f|
+        f.puts({ :user_token => user_token, :user_secret => user_secret }.to_yaml)
+      end
     end
 
     def self.get_credentials
+      YAML.load_file(CREDENTIALS_STORE)
     end
-    
+
     protected
     def self.has_stored_credentials?
+      FileUtils.exist? CREDENTIALS_STORE
     end
 
   end
