ffd6f4be0b2b1676abc41cb74fdb7653b4000741
Matthew Rothenberg <mrothenberg@gmail.com>
2013-03-11 18:26:29 -0400

    begin vcr testing for fitbit

--- spec/fitbit_spec.rb
+++ spec/fitbit_spec.rb
@@ -1,24 +1,34 @@
 require 'spec_helper'
 include Howami
 
-describe Fitbit, :vcr do
+describe Fitbit do
+  
+  use_vcr_cassette "fitbit"
 
   describe ".new" do
     it "should be instantiated without any args" do
-      lambda { Authentication.new() }.should_not raise_error
-      lambda { Authentication.new( "moo ") }.should raise_error(ArgumentError)
+      lambda { Fitbit.new() }.should_not raise_error
+      lambda { Fitbit.new( "moo ") }.should raise_error(ArgumentError)
     end
 
     it "should retrieve all the values it needs on instantiation"
   end
 
-  describe "#weight_str" do
-    it "returns a nicely formatted string with the proper weight" do
-      Fitbit.new.weight_str.should match(/Current weight:/)
+  context "output rendering" do
+    before(:each) { @fb = Fitbit.new }
+
+    describe "#weight_str" do
+      it "returns a nicely formatted string with the proper weight" do
+        @fb.weight_str.should match(/Current weight:\t\d\d\d\.\d pounds/)
+      end
+      it "returns a nice error message if there is no weight from past week" do
+        @fb.instance_variable_set("@weights",[])
+        @fb.weight_str.should match(/No weight data recorded in past 7 days./)
+      end
+      it "adds an indicator if measurement is more than 24hrs old"
     end
-    it "returns a nice error message if there is no weight from past week"
+    describe "#fat_str"
+    describe "#sleep_str"
   end
-  describe "#fat_str"
-  describe "#sleep_str"
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,20 @@
 require 'rspec'
 require 'howami'
-# require 'vcr'
+require 'vcr'
 
 RSpec.configure do |config|
   config.color_enabled = true
   config.formatter     = 'documentation'
+  config.extend VCR::RSpec::Macros
   # config.treat_symbols_as_metadata_keys_with_true_values = true
 end
 
-# VCR.configure do |c|
-#   c.cassette_library_dir = 'spec/cassettes'
-#   c.hook_into :webmock
-#   c.configure_rspec_metadata!
-# end
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.default_cassette_options = { :record => :new_episodes }
+  c.filter_sensitive_data('<OAUTH_AUTH_HEADER>') do |interaction|
+    interaction.request.headers['Authorization'].first
+  end
+  # c.configure_rspec_metadata!
+end
