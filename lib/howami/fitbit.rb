672719bd2be6b94a30a47a26763af8a5921a5aa1
Matthew Rothenberg <mrothenberg@gmail.com>
2013-05-10 15:57:29 -0400

    ugly workaround for upstream bug in fitgem
    should restore ruby 1.8.7 compatibility

--- lib/howami/fitbit.rb
+++ lib/howami/fitbit.rb
@@ -16,11 +16,19 @@ module Howami
       @fats       = @client.data_by_time_range('/body/log/fat', {:base_date => Date.today.to_s, :period => '1w'})['fat']
       @sleeps     = @client.sleep_on_date('today')
       @activities = @client.activities_on_date('today')
+      @weight_lbl = weight_lbl_value_187_workaround
+    end
+
+    # this method breaks the fitgem gem in ruby 1.8.7, so we just work around it and dont
+    # offer any i18n support for default units in 1.8.7, just assume AMERICA FUCK YEAH
+    def weight_lbl_value_187_workaround
+      return "pounds" if RUBY_VERSION =~ /^1\.8/
+      @client.label_for_measurement(:weight)
     end
 
     def weight_str
       if @weights.last
-        return "#{@weights.last['weight'].to_s} #{@client.label_for_measurement(:weight)}"
+        return "#{@weights.last['weight']} #{@weight_lbl}"
       else
         return "* No weight data recorded in past 7 days."
       end
